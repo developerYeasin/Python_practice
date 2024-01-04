b9ae2a6c1bea6958ea593708ce7b72971582a8cc
developerYeasin <yea13sin@gmail.com>
2024-01-04 15:45:39 +0600

    complete chapter 6

---
+++ chapter6/01_conditional.py
@@ -0,0 +1,18 @@
+# a = 2
+# if(a>7): 
+#     print("Then value of a a is greater than 7")
+# elif(a>3):
+#     print("The value of a is greater than 3")
+# else:
+#     print("The value is not greater than 3 or")
+
+
+a = None
+a = 0
+if(a is None):
+    print("Yes")
+else:
+    print("No")
+
+b =[23,34,54]
+print(23 in b)
