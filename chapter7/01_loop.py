c921b94d11ca0f0f0207856e482f86825a7539c2
developerYeasin <yea13sin@gmail.com>
2024-01-05 14:46:07 +0600

    complete chapter 7

---
+++ chapter7/01_loop.py
@@ -0,0 +1,40 @@
+# ===============================
+#           while Loop
+#================================
+
+# i = 0
+# while i<=100:
+#     print("Yes", i)
+#     i+=1
+
+
+# fruits = ['Banana', 'Watermelon', 'Grapes', 'Mangoes']
+
+# i=0
+# while i<len(fruits):
+#     print(fruits[i])
+    # i+=1
+
+
+# ===============================
+#           for Loop
+#================================
+
+fruits = ['Banana', 'Watermelon', 'Grapes', 'Mangoes']
+
+i=0
+for item in fruits:
+    print(item, "item")
+
+
+# for i in range(9):
+#     print(i)
+# else:
+#     print("end of the for")
+
+for i in range(9):
+    print(i)
+    if i == 10:
+        break
+else:
+    print("end of the for")
